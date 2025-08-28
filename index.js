d2f6d85a01c1b0735ddffa06aa7cc09e283f8604
Sweety p <sweetyparaman123@gmail.com>
2025-08-27 20:29:57 -0700

    Initial commit for Render deployment

--- index.js
+++ index.js
@@ -128,4 +128,4 @@ app.patch("/meetings/:id", (req, res) => {
   );
 });
 
-app.listen(PORT, () => console.log(`Server running on http://localhost:${PORT}`));
+app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
