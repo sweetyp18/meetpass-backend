30078ab84a317ab3df9b32f7cf27ab8dee7a4690
sweetyp18 <sweetyparaman123@gmail.com>
2025-10-09 19:34:04 +0530

    Add files via upload

--- index.js
+++ index.js
@@ -9,6 +9,7 @@ const cors = require("cors");
 const app = express();
 
 // ---------- Middleware ----------
+app.use(express.json());
 app.use(cors({
   origin: "http://localhost:3000",
   methods: ["GET", "POST", "PATCH", "DELETE"],
